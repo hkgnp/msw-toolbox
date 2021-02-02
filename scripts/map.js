08e1229d60c157740484a01f35fa3fe4939d4b8f
hkgnp <ben@pngs.cc>
2021-02-02 14:58:09 +0800

    Sucessful nearest search

--- scripts/map.js
+++ scripts/map.js
@@ -29,19 +29,38 @@ document.querySelector('#whereami').addEventListener('click', () => {
 
   getLocation();
 });
+let searchLayer = [];
 window.addEventListener('DOMContentLoaded', () => {
   (async () => {
     let response = await axios.get('geojson/sso.geojson');
-    let layer = L.geoJson(response.data, {
+    layer = L.geoJson(response.data, {
       onEachFeature: (feature, layer) => {
         new L.marker([
           feature.geometry.coordinates[1],
           feature.geometry.coordinates[0],
         ]).addTo(map);
-
         layer.bindPopup(feature.properties.Description);
         layer.addTo(map);
+        searchLayer.push(layer);
       },
     });
   })();
+});
+
+document.querySelector('#findnearest').addEventListener('click', () => {
+  closestPt = L.GeometryUtil.closestLayer(map, searchLayer, currPosition);
+  console.log(searchLayer);
+  console.log(map);
+  console.log(currPosition);
+  console.log(closestPt);
+
+  // let myIcon = L.icon({ iconUrl: 'pin.png', iconAnchor: [29, 64] });
+  // L.marker([closestPt.latlng.lat, closestPt.latlng.lng], {
+  //   icon: myIcon,
+  // }).addTo(map);s
+
+  let popup = L.popup()
+    .setLatLng([closestPt.latlng.lat, closestPt.latlng.lng])
+    .setContent(closestPt.layer.feature.properties.Description)
+    .openOn(map);
 });
