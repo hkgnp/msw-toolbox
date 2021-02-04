0bc7e79ed4a3cb7a67b6bd364d72e7a39182a1bd
hkgnp <ben@pngs.cc>
2021-02-04 16:13:03 +0800

    Fixed convert radio value to array name and zoom on popup

--- scripts/map-functions.js
+++ scripts/map-functions.js
@@ -20,11 +20,71 @@ document.querySelector('#whereami').addEventListener('click', () => {
       .setContent('You are here!')
       .openOn(map);
   };
-
   getLocation();
 });
 
-// Find nearest using Current Position of User
+// Use "Postal Code" to find nearest services"
+document.querySelector('#submit-search').addEventListener('click', () => {
+  ///////////// SECTION: USER INPUTS /////////////
+
+  // Services selected
+  let allLayers = {
+    searchSsoLayer: searchSsoLayer,
+    searchDisabilityLayer: searchDisabilityLayer,
+    searchFscLayer: searchFscLayer,
+  };
+
+  let userService;
+  let serviceRadios = document.querySelectorAll('.services');
+  for (let service of serviceRadios) {
+    if (service.checked) {
+      userService = allLayers[service.value];
+      break;
+    }
+  }
+
+  // Postal code
+  let searchPostalCode = [];
+  let userPostalCode = document.querySelector('#postalcode');
+  (async () => {
+    let response = await axios.get(
+      'https://developers.onemap.sg/commonapi/search',
+      {
+        params: {
+          searchVal: userPostalCode.value,
+          returnGeom: 'Y',
+          getAddrDetails: 'Y',
+          pageNum: '1',
+        },
+      }
+    );
+
+    let postalCodeResults = response.data.results[0];
+    searchPostalCode = [
+      parseFloat(postalCodeResults.LATITUDE),
+      parseFloat(postalCodeResults.LONGITUDE),
+    ];
+
+    closestPt = L.GeometryUtil.closestLayer(map, userService, searchPostalCode);
+
+    L.popup()
+      .setLatLng([closestPt.latlng.lat, closestPt.latlng.lng])
+      .setContent(
+        closestPt.layer.feature.properties.Description ||
+          closestPt.layer.feature.properties.name
+      )
+      .openOn(map);
+
+    map.setView([closestPt.latlng.lat, closestPt.latlng.lng], 18);
+
+    let myIcon = L.icon({ iconUrl: 'pin.png', iconAnchor: [33, 68] });
+    L.marker([closestPt.latlng.lat, closestPt.latlng.lng], {
+      icon: myIcon,
+    }).addTo(map);
+  })();
+});
+
+// Use "Use My Location" to find nearest services
 document.querySelector('#findnearest').addEventListener('click', () => {
   closestPt = L.GeometryUtil.closestLayer(map, searchSsoLayer, currPosition);
 
