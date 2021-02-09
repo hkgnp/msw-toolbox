f97e25e54f02734b87dd0bdc5a74e3cf9b3e2780
hkgnp <ben@pngs.cc>
2021-02-09 21:26:42 +0800

    Separated chart renders from dataset retrieval

--- scripts/infographs.js
+++ scripts/infographs.js
@@ -1,38 +1,57 @@
-window.addEventListener('DOMContentLoaded', async () => {
-  // Load Tribunal Responses
-  let tribunalResponse = await loadTribunalData();
+////////// RENDER CHARTS //////////
 
-  const optionsTribunal = {
-    chart: {
-      type: 'line',
-      height: '100%',
-    },
-    series: [
-      {
-        name: 'Applications for Variation',
-        data: tribunalResponse.ySeriesApplicationsForVariation,
-      },
-      {
-        name: 'New Applications for Maintenance',
-        data: tribunalResponse.ySeriesNewApplicationsforMaintenance,
-      },
-    ],
+// Render Tribunal of Maintenance of Parents Chart
+const optionsTribunal = {
+  chart: {
+    type: 'line',
+    height: '100%',
+  },
+  series: [],
+  xaxis: {},
+  // the noData property allows us to define what to show
+  // if there is no data loaded
+  noData: {
+    text: 'Loading...',
+  },
+};
+
+const tribunalResponseChart = new ApexCharts(
+  document.querySelector('#tribunal'),
+  optionsTribunal
+);
+tribunalResponseChart.render();
+
+// Render Healthcare Attendances Chart
+
+// Render Child and Adult Protection Charts
+
+////////// AXIOS LOADED DATASETS //////////
+window.addEventListener('DOMContentLoaded', async () => {
+  // Load Tribunal Dataset
+  let tribunalResponse = await loadTribunalData();
+  tribunalResponseChart.updateOptions({
     xaxis: {
       categories: tribunalResponse.x,
     },
-    // the noData property allows us to define what to show
-    // if there is no data loaded
-    noData: {
-      text: 'Loading...',
+  });
+  tribunalResponseChart.updateSeries([
+    {
+      name: 'Applications for Variation',
+      data: tribunalResponse.ySeriesApplicationsForVariation,
     },
-  };
+    {
+      name: 'New Applications for Maintenance',
+      data: tribunalResponse.ySeriesNewApplicationsforMaintenance,
+    },
+  ]);
 
-  const chart = new ApexCharts(
-    document.querySelector('#tribunal'),
-    optionsTribunal
-  );
-  chart.render();
+  // Load healthcare dataset
+  let healthAttendancesResponse = await loadInpatientOutpatientAttendances();
 
-  // Load healthcare attendances
-  let healthAttendances = await healthAttendances();
+  let acuteHospitalAdmissions = [];
+  for (let r of healthAttendancesResponse) {
+    if (r.section == 'Acute Hospitals Admissions') {
+      acuteHospitalAdmissions.push(r);
+    }
+  }
 });
